4f21790e97e8d69c5822cc5f32b0a7e69a39ef2a
septianmy <haiseptian@gmail.com>
2021-01-11 06:16:19 +0700

    Setting Deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var cors = require("cors");
 const app = express()
-const port = 5001;
+const port = process.env.PORT || 5001;
 const bodyParser = require("body-parser");
 const router = require('./src/routes');
 app.use(express.json());
